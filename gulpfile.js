f4bcad23963f353200efecd2cf7bcc016cad0eaa
Geoff Doty <n2geoff@gmail.com>
2018-04-07 04:03:46 -0400

    added build process (gulp)

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+const gulp = require("gulp");
+const minify = require('gulp-minify');
+const strip = require("gulp-strip-comments");
+
+gulp.task("default", function() {
+    gulp.src("./src/testit.js")
+        .pipe(strip({safe: true}))
+        .pipe(minify({ext: {min: ".min.js"}}))
+        .pipe(gulp.dest("dist"))
+});

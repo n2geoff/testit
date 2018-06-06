22a49b4a75ce700352d170797f73599ee4abc39c
Geoff Doty <n2geoff@gmail.com>
2018-06-06 08:03:45 -0400

    added sample node test runner

---
+++ test/run.js
@@ -0,0 +1,24 @@
+var path = require("path");
+var fs = require("fs");
+
+fs.readdir(__dirname, function(err, files) {
+    if(err) {
+        process.exit(1);
+    }
+
+    var tests = files.filter(function(item) {
+        return item.indexOf("spec.js") !== -1; 
+    });
+
+    tests.forEach(function(file) {
+
+        console.log(`: ${file}`);
+
+        var me = fs.readFileSync(path.join(__dirname, file))
+
+        // eval maybe evil, but its your code, are you evil?
+        eval(me.toString());
+
+        test.run();
+    });
+});
